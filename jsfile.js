fa3e6907d5bb04885b4ae75a7f52eadcca5869e0
Haruna Seidu <69992226+Haruna245@users.noreply.github.com>
2021-09-06 15:18:50 -0100

    Add files via upload

---
+++ jsfile.js
@@ -0,0 +1,18 @@
+let mail = "seidu"; //line 1 and 2 is a temporal variable for holding email and password
+let pass = "admin";
+
+
+
+function con_firm(){ // i was testing the mail and pass variables
+    if(document.getElementById("name").value == mail && document.getElementById("psd").value == pass ){
+        window.open("signUpfile.html");
+                }
+        }    
+    
+
+function Sign_Up() {
+    window.open("signUpfile.html");
+    }
+        
+
+                        
